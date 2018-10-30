c0e7e48973b969985760e719733739627150d7f0
Daniel Phillips <daniel.j.phillips@gmail.com>
2018-10-30 15:33:18 -0700

    Add pipfile for dependency tracking. Created models and set up db, admin

--- project/review/models.py
+++ project/review/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Movie(models.Model):
+    imdb_id = models.TextField(default='')
+    plot = models.TextField(default='')
+    runtime = models.TextField(default='')
+    rated = models.TextField(default='Unknown')
+    title = models.TextField(default='')
+    year = models.IntegerField(default=1900)
+
+    def __str__(self):
+        return self.title
+
+
+class Firstview(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    firstview_text = models.CharField(max_length=200)
+    rating = models.IntegerField(default = 0)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.firstview_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    review_text = models.CharField(max_length=200)
+    rating = models.IntegerField(default = 0)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.review_text
+
+
